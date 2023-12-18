4b8328ba7191db96609b3fd0adf887304f3642f1
MuhamadAzmiNaziyulloh <azmi89017@gmail.com>
2023-12-18 21:46:21 +0700

    tugas

---
+++ script.js
@@ -0,0 +1,52 @@
+let bilangan = 21
+if(bilangan % 2==0){
+    hasil = 'bilangan genap'
+}else {
+    console.log('ganjil')
+    hasil = 'bilangan ganjil'
+}
+
+
+let alas = 12 
+let tinggi = 15 
+
+
+let penambahan =  1/2 *tinggi * alas 
+console.log(`luas lingkaran adalah ${penambahan}`)
+let phi = 3.14
+let diamater = 20
+let d = Math.round(phi * diamater)
+console.log(`luas segitiga adalah ${d}`)
+
+
+
+let p = 40
+let l = 25
+let h = p * l
+console.log(`luas persegi panjang adalag ${h}`)
+
+
+
+// let nilaiSiswa = prompt('')
+// if(nilaiSiswa > 90){
+//     alert('grade A')
+// }else if (nilaiSiswa > 80){
+//     alert('grade B ')
+// }else if (nilaiSiswa > 70){
+//     alert('grade c')
+// }else {
+//     alert('errorr')
+// }
+
+
+
+
+let lampu = 0
+if(lampu == 0){
+    terjadi = 'lampu nyala'
+}else {
+    terjadi = 'lampu mati'
+}
+console.log(terjadi)
+
+
---
+++ tugas.js
@@ -0,0 +1,37 @@
+
+// penjumlahan angka 2
+
+let bil1 = 20;
+let bil2 = 30;
+let hasil = bil1 + bil2;
+console.log(hasil);
+
+
+
+// menghitung jam ke detik
+let jam = 2
+hasill = jam * 3600
+console.log(hasill)
+
+//  menghitung jam ke menit
+
+let jam2 = 3
+let jawaban = jam2 * 60
+console.log(jawaban)
+
+
+// menghitung kg ke gram
+let kg = 20
+let penyelesain = 20 * 1000
+console.log(`${penyelesain} g`)
+
+
+// menghitung km ke meter 
+let km = 3
+let jumlahMeter = km * 1000
+console.log(jumlahMeter)
+
+
+
+
+
